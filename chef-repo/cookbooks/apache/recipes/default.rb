695a5c1d04632a8240a1e4dfa65452b18758687e
Kohn Om <Kohn.Om@blackboard.com>
2014-02-25 15:35:03 -0500

    added template

--- chef-repo/cookbooks/apache/recipes/default.rb
+++ chef-repo/cookbooks/apache/recipes/default.rb
@@ -16,8 +16,8 @@ service "httpd" do
 	action [ :enable, :start ]
 end
 
-cookbook_file "/var/www/html/index.html" do
-	source "index.html"
+template "/var/www/html/index.html" do
+	source "index.html.erb"
 	mode "0644"
 end
 
