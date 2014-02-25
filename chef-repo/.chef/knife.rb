519918404d9c2e3bd47bed139a9368437340f777
Kohn Om <Kohn.Om@blackboard.com>
2014-02-25 13:04:30 -0500

    added author name

--- chef-repo/.chef/knife.rb
+++ chef-repo/.chef/knife.rb
@@ -1,5 +1,5 @@
 # See http://docs.opscode.com/config_rb_knife.html for more information on knife configuration options
-
+# Kohn Om
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
